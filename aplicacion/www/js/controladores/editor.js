241b1e074668a457430a415f7a9d9e13a041e99a
EliteArmy <ariel9501@yahoo.com>
2019-03-03 15:11:29 -0600

    leer un archivo en el editor

--- aplicacion/www/js/controladores/editor.js
+++ aplicacion/www/js/controladores/editor.js
@@ -1,5 +1,5 @@
-var txtAreas = ["html", "css", "javascript", "html-resultado"]
-var modo = ["text/html", "css", "javascript", "text/html"]
+var txtAreas = ["html", "css", "javascript", "html-resultado"];
+var modo = ["text/html", "css", "javascript", "text/html"];
 var editores = [];
 
 for (var i = 0; i<txtAreas.length; i++){
@@ -7,9 +7,10 @@ for (var i = 0; i<txtAreas.length; i++){
     mode: modo[i],
     lineNumbers: true,
     tabSize: 2,
+    value: "CSS",
     cursorScrollMargin: 20,
     scrollbarStyle: "overlay"
-  });
+    });
 }
 
 var input = document.getElementById("select");
@@ -37,6 +38,40 @@ CodeMirror.on(window, "hashchange", function() {
   var theme = location.hash.slice(1);
   if (theme) { input.value = theme; selectTheme(); }
 });
+
+
+/* Test */
+/*$("#generar").click(function(){
+  alert("Generando Html");
+  
+  editores[0].setValue(`<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Page Title</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+      <p>Hola Mundo!</p>
+    </body>
+  </html>`);
+
+  alert(editores[0].getValue())
+
+});*/
+
+function loadfile(input) {
+  var reader = new FileReader();
+  reader.onload = function(e) {
+      editores[0].setValue(e.target.result);
+  }
+  reader.readAsText(input.files[0]);
+}
+
+
+
+
 
 
 

284ec58f4f19f1c430198ff597ea656c2b94f632
EliteArmy <ariel9501@yahoo.com>
2019-04-26 15:13:49 -0600

    inicio de node

--- aplicacion/index.js
+++ aplicacion/index.js
@@ -1,14 +1,23 @@
+'use strict'
+
 // Importar el modulo express para crear el servidor web
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser');
 
 // Crear una aplicacion de nodejs con express
 var app = express();
+const port = process.env.PORT || 3000
 
 // Definir una carpeta como publica para que los usuarios puedan acceder a su contenido
 app.use(express.static("www"));
 
-//Levantar el servidor en el puerto 3333
-app.listen(3333, function(){
-  console.log("servidor levantado en el puerto 3333");
-});
+// Poder parsear el cuerpo de la peticion y poder tratar los datos que enviemos
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()) // poder admitir peticiones con cuerpo de msj en formato json
 
+// Otra forma de escribirlo más resumido y con variable port:
+app.listen (port, () => {
+  console.log(`Api rest corriendo en http://localhost: ${port}`)
+})
+
+// Correrlo con nodemon instalado: node index.js
